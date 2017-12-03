43978e0986e583f0f626dab94b1e9adb694e32d8
Florian Posdziech <hallo@flowfx.de>
2017-12-03 07:33:19 -0600

    Solve 2nd challenge
    Use simplified list of tuples, sort by updated_at,
    print simple list.

--- repocount.py
+++ repocount.py
@@ -10,6 +10,7 @@ the following questions about Customer.io's public repositories:
 3. Which repository has the most watchers?
 """
 import requests
+from operator import itemgetter
 
 org = 'customerio'
 repos_url = f'https://api.github.com/orgs/{org}/repos'
@@ -17,9 +18,18 @@ repos_url = f'https://api.github.com/orgs/{org}/repos'
 req = requests.get(repos_url)
 
 # get a Python dict 
-repos = req.json()
+repos_dict = req.json()
 
 # Sum up total open issues across all repos.
-total_open_issues = sum([repo['open_issues_count'] for repo in repos])
+total_open_issues = sum([repo['open_issues_count'] for repo in repos_dict])
 
 print(f'Total open issues: {total_open_issues}')
+
+repo_tuples = [(x['name'], x['updated_at'], x['open_issues_count'], x['watchers_count']) for x in repos_dict]
+
+# https://docs.python.org/3/howto/sorting.html
+# sort by updated_at
+sorted_repos = sorted(repo_tuples, key=itemgetter(1), reverse=True)
+
+for r in sorted_repos:
+    print(f'{r[0]}, last updated at: {r[1]}')
