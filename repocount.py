f64d68a47789103754307b16b6095d0f11a0982f
Florian Posdziech <hallo@flowfx.de>
2017-12-03 07:43:56 -0600

    Prettify printout

--- repocount.py
+++ repocount.py
@@ -15,15 +15,15 @@ from operator import itemgetter
 org = 'customerio'
 repos_url = f'https://api.github.com/orgs/{org}/repos'
 
+# get list of public repositories
 req = requests.get(repos_url)
 
-# get a Python dict 
+# make it a Python dict 
 repos_dict = req.json()
 
 # Sum up total open issues across all repos.
 total_open_issues = sum([repo['open_issues_count'] for repo in repos_dict])
 
-print(f'Total open issues: {total_open_issues}')
 
 repo_tuples = [(x['name'], x['updated_at'], x['open_issues_count'], x['watchers_count']) for x in repos_dict]
 
@@ -31,10 +31,12 @@ repo_tuples = [(x['name'], x['updated_at'], x['open_issues_count'], x['watchers_
 # sort by updated_at
 sorted_repos = sorted(repo_tuples, key=itemgetter(1), reverse=True)
 
-for r in sorted_repos:
-    print(f'{r[0]}, last updated at: {r[1]}')
 
 sorted_by_watchers = sorted(repo_tuples, key=itemgetter(3), reverse=True)
 most_watched = sorted_by_watchers[0]
 
+print(f'Total open issues: {total_open_issues}')
 print(f'Most watched repo: {most_watched[0]}, watchers: {most_watched[3]}')
+print('\nAll repos:')
+for r in sorted_repos:
+    print(f'{r[0]:30} updated: {r[1]}')
